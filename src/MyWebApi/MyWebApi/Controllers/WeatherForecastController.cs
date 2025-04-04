b6e197aee4b29e79906a02758b921ca2529c076a
LiteObject <Lite.Object@gmail.com>
2025-04-04 14:27:18 -0500

    Fix message formatting in InfoController's Get method

--- src/MyWebApi/MyWebApi/Controllers/WeatherForecastController.cs
+++ src/MyWebApi/MyWebApi/Controllers/WeatherForecastController.cs
@@ -17,7 +17,7 @@ namespace MyWebApi.Controllers
         public IActionResult Get()
         {
             // Log a message at the Information level
-            var message = "Hello World from InfoController...";
+            var message = "Hello World from InfoController. This is a sample message.";
             _logger.LogInformation(message);
             return Ok(message);
         }
